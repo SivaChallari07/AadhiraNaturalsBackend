d5670475279bc19ec2cacfc2f119abcd21038954
Sivarama krishna Challari <sivaramakrishnachallari1277@gmail.com>
2025-02-26 15:47:54 +0530

    Add files via upload

---
+++ AadhiraNaturalsApplication.java
@@ -0,0 +1,13 @@
+package com.aadhira.naturals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadhiraNaturalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadhiraNaturalsApplication.class, args);
+	}
+
+}
